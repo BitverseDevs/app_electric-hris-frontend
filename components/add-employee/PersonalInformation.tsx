acc89347509acbe78be18db7193b037e9f4fa104
jhumer-apus <jhumerapus@gmail.com>
2024-06-07 17:02:15 +0800

    Continue working on personal information

--- components/add-employee/PersonalInformation.tsx
+++ components/add-employee/PersonalInformation.tsx
@@ -1,9 +1,66 @@
-import React from 'react';
-import { Card, FormProps } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, FormProps, Row } from 'antd';
 import { Button, Form, Input, Select, Space, DatePicker } from 'antd';
+import { PersonalData } from '@/types';
+import type { DatePickerProps } from 'antd';
+
 
 export default function PersonalInformation() {
+
+    //STATES
+    const [personalData, setPersonalData] = useState<PersonalData>(
+        {
+            first_name: '',
+            middle_name: '',
+            last_name: '',
+            suffix: '',
+            birth_date: null,
+            sex: '',
+            civil_status: '',
+            spouse_first_name: '',
+            spouse_middle_name: '',
+            spouse_last_name: '',
+            spouse_suffix: '',
+        }
+    );
+
     const [form] = Form.useForm();
+
+    //USE EFFECTS
+    useEffect(() => {
+        console.table(personalData)
+    },[personalData])
+
+    //FUNCTIONS
+    const handleChangePersonalData = (e:any) => {
+
+        setPersonalData((curr:PersonalData) => (
+            {
+                ...curr,
+                [e.target.name]: e.target.value
+            }
+        ))
+    }
+
+    const handleSelectChange = (name:string, value:string | number | null) => {
+        if(value) {
+            setPersonalData((curr:PersonalData) => (
+                {
+                    ...curr,
+                    [name]: value
+                }
+            ))
+        }
+    }
+
+    const handleDateChange = (name:string, date: Date, dateString:string | string[] | null) => {
+        setPersonalData((curr:PersonalData) => (
+            {
+                ...curr,
+                [name]: dateString
+            }
+        ))
+    }
 
     const onFinish: FormProps['onFinish'] = (values) => {
         console.log('Success:', values);
@@ -17,119 +74,247 @@ export default function PersonalInformation() {
                 initialValues={{ remember: true }}
                 onFinish={onFinish}
             >
-                <Space wrap>
-                    <Form.Item 
-                        name="first_name" 
-                        label="First Name:" 
-                        rules={
-                            [
+                <Row gutter={16}>
+                    <Col xs={24} sm={12} md={8} lg={4}>
+                        <Form.Item 
+                            name="first_name" 
+                            label="First Name:" 
+                            rules={[
                                 { 
                                     required: true,
                                     message: 'Please input First Name' 
                                 },
-                                
-                            ]
-                        }
-                    >
-                        <Input />
-                    </Form.Item>
+                            ]}
+                        >
+                            <Input 
+                                name="first_name"
+                                onChange={handleChangePersonalData}
+                            />
+                        </Form.Item>
+                    </Col>
 
-                    <Form.Item 
-                        name="middle_name" 
-                        label="Middle Name:" 
-                        rules={
-                            [
+                    <Col xs={24} sm={12} md={8} lg={4}>
+                        <Form.Item 
+                            name="middle_name" 
+                            label="Middle Name:" 
+                            rules={[
                                 { 
                                     required: true,
                                     message: 'Please input Middle Name' 
                                 }
-                            ]
-                        }
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item 
-                        name="last_name" 
-                        label="Last Name:" 
-                        rules={
-                            [
+                            ]}
+                        >
+                            <Input
+                                name="middle_name" 
+                                onChange={handleChangePersonalData}
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    <Col xs={24} sm={12} md={8} lg={4}>
+                        <Form.Item 
+                            name="last_name" 
+                            label="Last Name:" 
+                            rules={[
                                 { 
                                     required: true,
                                     message: 'Please input Last name' 
                                 }
-                            ]
-                        }
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item 
-                        name="suffix" 
-                        label="Suffix:" 
-                        rules={
-                            [
-                                { 
-                                    required: true,
-                                    message: 'Please input Suffix' 
-                                }
-                            ]
-                        }
-                    >
-                        <Input />
-                    </Form.Item>
-                </Space>
-                <br></br>
-                <Space wrap>
-                    <Form.Item 
-                        name="birth_date" 
-                        label="Birth Date:" 
-                        rules={
-                            [
+                            ]}
+                        >
+                            <Input 
+                                name="last_name" 
+                                onChange={handleChangePersonalData}
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    <Col xs={24} sm={12} md={8} lg={4}>
+                        <Form.Item 
+                            name="suffix" 
+                            label="Name Suffix:" 
+                        >
+                            <Input 
+                                name="suffix" 
+                                onChange={handleChangePersonalData}
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    <Col xs={24} sm={12} md={8} lg={4}>
+                        <Form.Item 
+                            name="birth_date" 
+                            label="Birth Date:"
+                            rules={[
                                 { 
                                     required: true,
                                     message: "Please input Birth Date" 
                                 },
-                                
-                            ]
-                        }
-                    >
-                        <DatePicker />
-                    </Form.Item>
-
-                    <Form.Item 
-                        name="birth_place" 
-                        label="Birth Place:" 
-                        rules={
-                            [
-                                { 
-                                    required: true,
-                                    message: 'Please input Birth Place' 
-                                }
-                            ]
-                        }
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item 
-                        name="sex" 
-                        label="Sex:" 
-                        rules={
-                            [
-                                { 
-                                    required: true,
-                                    message: 'Please input Sex' 
-                                }
-                            ]
-                        }
-                    >
-                        <Select
-                            // onChange={handleChange}
-                            options={[
-                                { value: 'male', label: 'Male' },
-                                { value: 'female', label: 'Female' },
                             ]}
-                        />
-                    </Form.Item>
-                </Space>
+                        >
+                            <DatePicker 
+                                className='w-full'
+                                name="birth_date" 
+                                onChange={(date:Date, dateString:string | string []) => handleDateChange('birth_date', date, dateString)}
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    <Col xs={24} sm={12} md={8} lg={4}>
+                        <Form.Item 
+                            name="sex" 
+                            label="Sex:" 
+                            rules={[
+                                { 
+                                    required: true,
+                                    message: 'Please Select Sex' 
+                                }
+                            ]}
+                        >
+                            <Select 
+                                onChange={(value:string) => handleSelectChange("sex",value)}
+                                options={[
+                                    { value: 'male', label: 'Male' },
+                                    { value: 'female', label: 'Female' },
+                                ]}
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    <Col span={24}>
+                        <Row align="bottom" gutter={16}>       
+                            <Col xs={24} sm={24} md={4} >
+                                <Form.Item 
+                                    name="civil_status" 
+                                    label="Civil Status:"
+                                    className='w-full'
+                                    rules={[
+                                        { 
+                                            required: true,
+                                            message: 'Please Select Civil Status' 
+                                        }
+                                    ]}
+                                >
+                                    <Select
+                                        onChange={(value:string) => handleSelectChange("civil_status",value)}
+                                        options={[
+                                            { value: 'S', label: 'Single' },
+                                            { value: 'M', label: 'Married' },
+                                            { value: 'W', label: 'Widowed' },
+                                            { value: 'A', label: 'Annulled' },
+                                        ]}
+                                    />
+                                </Form.Item>
+                            </Col>
+                            
+                            <Col flex="auto">
+                                <Form.Item 
+                                    name="spouse_first_name" 
+                                    label="Spouse First Name:"
+                                    className='w-full'
+                                    rules={
+                                        personalData.civil_status == "M" ? [
+                                            { 
+                                                required: true,
+                                                message: 'Please input Spouse First Name' 
+                                            },
+                                        ]: []
+                                    }
+                                >
+                                    <Input 
+                                        name="spouse_first_name" 
+                                        disabled={personalData.civil_status != "M"}
+                                        onChange={handleChangePersonalData}
+                                    />
+                                </Form.Item>
+                            </Col>
+                            
+                            <Col flex="auto">
+                                <Form.Item 
+                                    name="spouse_middle_name" 
+                                    label="Spouse Middle Name:"
+                                    className='w-full'
+                                    rules={
+                                        personalData.civil_status == "M" ? [
+                                            { 
+                                                required: true,
+                                                message: 'Please input Spouse Middle Name' 
+                                            },
+                                        ]: []
+                                    }
+                                >
+                                    <Input 
+                                        name="spouse_middle_name" 
+                                        disabled={personalData.civil_status != "M"}
+                                        onChange={handleChangePersonalData}
+                                    />
+                                </Form.Item>
+                            </Col>
+                            
+                            <Col flex="auto">
+                                <Form.Item 
+                                    name="spouse_last_name" 
+                                    label="Spouse Last Name:"
+                                    className='w-full'
+                                    rules={
+                                        personalData.civil_status == "M" ? [
+                                            { 
+                                                required: true,
+                                                message: 'Please input Spouse Last Name' 
+                                            },
+                                        ]: []
+                                    }
+                                >
+                                    <Input 
+                                        name="spouse_last_name" 
+                                        disabled={personalData.civil_status != "M"}
+                                        onChange={handleChangePersonalData}
+                                    />
+                                </Form.Item>
+                            </Col>
+ 
+                            <Col flex="auto">
+                                <Form.Item 
+                                    name="spouse_suffix" 
+                                    label="Spouse Suffix:"
+                                    className='w-full'
+                                >
+                                    <Input 
+                                        name="spouse_suffix" 
+                                        disabled={personalData.civil_status != "M"}
+                                        onChange={handleChangePersonalData}
+                                    />
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xs={24} sm={12} md={8} lg={4}>
+                        <Form.Item 
+                            name="mobile_number" 
+                            label="Mobile Number:" 
+                            rules={[
+                                { 
+                                    required: true,
+                                    message: 'Please input Mobile Number' 
+                                },
+                                {
+                                    pattern: /^[0-9]{11}$/,
+                                    message: 'Please enter a valid 11-digit mobile number'
+                                }
+                            ]}
+                        >
+                            <Input
+                                name="mobile_number" 
+                                placeholder='09123456789'
+                                type="number"
+                                maxLength={11}
+                                onChange={handleChangePersonalData}
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                    
+
 
                 <Form.Item>
                     <Button type="primary" htmlType="submit">
---
+++ types/index.ts
@@ -0,0 +1,13 @@
+export interface PersonalData {
+    first_name: string | null,
+    middle_name: string | null,
+    last_name: string | null,
+    suffix: string | null,
+    birth_date: Date | null,
+    sex: string | null,
+    civil_status: string | null,
+    spouse_first_name: string | null,
+    spouse_middle_name: string | null,
+    spouse_last_name: string | null,
+    spouse_suffix: string | null,
+}
