bb0267d8fea70c98a78d86ed06ff324d711a3168
jhumer-apus <jhumerapus@gmail.com>
2024-08-09 17:37:58 +0800

    Update payslip

--- components/payrolls/payslips/ViewPayslip.tsx
+++ components/payrolls/payslips/ViewPayslip.tsx
@@ -16,33 +16,6 @@ export default function ViewPayslip(props: Props) {
     const {showPayslipModal, setModal} = useModalStore(state => state)
     const payslipRef = useRef<HTMLDivElement>(null);
 
-    const [payslipData, setPayslipData] = useState<any>({
-        employeeDetails: {
-            "Employee Name": "Boyax Yarn",
-            "Work Hours": 100
-        },
-        salaryDetails: {
-            "Basic Pay": 10000,
-            "Allowance Pay": 10000,
-            "Overtime Pay": 10000,
-            "Night Diff Pay": 1000,
-            "SL Encashment": 10000,
-            "VL Encashment": 100000,
-            "Bonus": 10000,
-            "Total Income": 900000,
-        },
-        paymentDetails: {
-            "Cut Off Period": "Jan 10 - Feb 25"
-        },
-        deductionDetails: {
-            "SSS": 2323,
-            "Pag-Ibig": 232323,
-            "Philhealth":1232,
-            "With Holding Tax": 122112,
-            "Total Decution":21232323
-        },
-    });
-
     const handlePrint = useReactToPrint({
         content: () => payslipRef.current,
     });
@@ -76,74 +49,157 @@ export default function ViewPayslip(props: Props) {
             // onClose={handleClose}
             onCancel={handleClose}
             footer={footer}
-            width={800}
+            width={1200}
         >
+
             {/* <Button type="primary" onClick={downloadPDF}>Download</Button> */}
             <Row ref={payslipRef} className="border-black border my-8 overflow-x-auto">
                 <Col span={8} className='border-black border'>
 
-                    <Row className='bg-slate-200 font-bold bg-blue-100 '>Company Name</Row>
+                    <Row className='bg-slate-200 font-bold bg-blue-100 p-2'>Company Name</Row>
 
                     <Row className='border-black border-y'>
-                        <Row gutter={12} className='w-full'>
+                        <Row gutter={12} className='w-full px-2'>
                             <Col span={12}>Employee Name:</Col>
                             <Col span={12}>Boyaxasss ssass</Col>
                         </Row>
-                        <Row gutter={12} className='w-full'>
+                        <Row gutter={12} className='w-full px-2'>
                             <Col span={12}>Work Hours:</Col>
                             <Col span={12}>10000</Col>
                         </Row>
                     </Row>
 
                     <Row className='w-full border-black border-y '>
-                        <Row gutter={16} className='w-full'>
+                        <Row gutter={16} className='w-full px-2'>
                             <Col span={12}>Basic Pay:</Col>
                             <Col span={12}>1000.00</Col>
                         </Row>
 
-                        <Row className='w-full'>
+                        <Row className='w-full px-2'>
                             <Col span={12}>Allowances:</Col>
                             <Col span={12}>9000.00</Col>
                         </Row>
 
-                        <Row className='w-full'>
+                        <Row className='w-full px-2'>
                             <Col span={12}>Overtime Pay:</Col>
                             <Col span={12}>5000.00</Col>
                         </Row>
 
-                        <Row className='w-full'>
+                        <Row className='w-full px-2'>
                             <Col span={12}>Overtime Pay:</Col>
                             <Col span={12}>5000.00</Col>
                         </Row>
 
-                        <Row className='w-full'>
+                        <Row className='w-full px-2'>
                             <Col span={12}>SL Encashment:</Col>
                             <Col span={12}>5000.00</Col>
                         </Row>
 
-                        <Row className='w-full'>
+                        <Row className='w-full px-2'>
                             <Col span={12}>VL Encashment:</Col>
                             <Col span={12}>2000.00</Col>
                         </Row>
 
-                        <Row className='w-full'>
+                        <Row className='w-full px-2'>
                             <Col span={12}>Bonus:</Col>
                             <Col span={12}>2200.00</Col>
                         </Row>
                     </Row>
 
                     <Row className='w-full border-black border-y'>
-                        <Row className='w-full bg-blue-100'>
+                        <Row className='w-full bg-blue-100 px-2'>
                             <Col span={12}>Total:</Col>
                             <Col span={12}>1000000.00</Col>
                         </Row>
                     </Row>
                 </Col>
+
                 <Col span={8} className='border-black border'>
-                
+                    <Row className='bg-slate-200 font-bold bg-blue-100 p-2'>Payslip</Row>
+
+                    <Row className='border-black border-y w-full'>
+                        <Row gutter={12} className='w-full px-2'>
+                            <Col span={12}>Cut off period:</Col>
+                            <Col span={12}>Boyax cut off</Col>
+
+                        </Row>
+                        <Row gutter={12} className='w-full px-2'>
+                            <br></br>        
+                        </Row>
+                    </Row>
+
+                    <Row className='w-full border-black border-y'>
+                        <Row className='w-full px-2'>
+                            <Col span={12}>SSS Contribution:</Col>
+                            <Col span={12}>1000000.00</Col>
+                        </Row>
+                        <Row className='w-full px-2'>
+                            <Col span={12}>PHIC Contribution:</Col>
+                            <Col span={12}>9000.00</Col>
+                        </Row>
+                        <Row className='w-full px-2'>
+                            <Col span={12}>HDMF Contribution:</Col>
+                            <Col span={12}>9300.00</Col>
+                        </Row>
+                        <Row className='w-full px-2'>
+                            <Col span={12}>With Holding Tax:</Col>
+                            <Col span={12}>500.00</Col>
+                        </Row>
+                        <Row className='w-full px-2'>
+                            <Col span={12}>Other Deductions:</Col>
+                            <Col span={12}>2340.00</Col>
+                        </Row>
+                    </Row>
+                    <Row className='w-full border-black border-t-y'>
+                        <Row className='w-full bg-blue-100 px-2'>
+                            <Col span={12}>Total Deductions:</Col>
+                            <Col span={12}>1000000.00</Col>
+                        </Row>
+                    </Row>
                 </Col>
+
                 <Col span={8} className='border-black border'>
-                
+                    <Row className='bg-slate-200 font-bold bg-blue-100 p-2'>Payslip</Row>
+
+                    <Row className='border-black border-y w-full'>
+                        <Row gutter={12} className='w-full px-2'>
+                            <Col span={12}>Cut off period:</Col>
+                            <Col span={12}>Boyax cut off</Col>
+
+                        </Row>
+                        <Row gutter={12} className='w-full px-2'>
+                            <br></br>        
+                        </Row>
+                    </Row>
+
+                    <Row className='w-full border-black border-y'>
+                        <Row className='w-full px-2'>
+                            <Col span={12}>SSS Contribution:</Col>
+                            <Col span={12}>1000000.00</Col>
+                        </Row>
+                        <Row className='w-full px-2'>
+                            <Col span={12}>PHIC Contribution:</Col>
+                            <Col span={12}>9000.00</Col>
+                        </Row>
+                        <Row className='w-full px-2'>
+                            <Col span={12}>HDMF Contribution:</Col>
+                            <Col span={12}>9300.00</Col>
+                        </Row>
+                        <Row className='w-full px-2'>
+                            <Col span={12}>With Holding Tax:</Col>
+                            <Col span={12}>500.00</Col>
+                        </Row>
+                        <Row className='w-full px-2'>
+                            <Col span={12}>Other Deductions:</Col>
+                            <Col span={12}>2340.00</Col>
+                        </Row>
+                    </Row>
+                    <Row className='w-full border-black border-t-y'>
+                        <Row className='w-full bg-blue-100 px-2'>
+                            <Col span={12}>Total Deductions:</Col>
+                            <Col span={12}>1000000.00</Col>
+                        </Row>
+                    </Row>
                 </Col>
             </Row>
         </Modal>
